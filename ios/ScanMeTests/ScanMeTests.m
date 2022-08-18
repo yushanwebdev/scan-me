8fd5e8017ab1c7ed172a7a24b04de49a3073fad3
yushan <yushanwebdev@gmail.com>
2022-08-18 11:38:05 +0530

    feat: Rename the app - Rename the app to "Scan Me"

--- ios/ScanMeTests/ScanMeTests.m
+++ ios/ScanMeTests/ScanMeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface BlottNativeBoilerplateTests : XCTestCase
+@interface ScanMeTests : XCTestCase
 
 @end
 
-@implementation BlottNativeBoilerplateTests
+@implementation ScanMeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
