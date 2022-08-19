684eaa5da0e6da8d7c7c4bcbf24d3bb504b5ea4d
yushan <yushanwebdev@gmail.com>
2022-08-19 10:33:20 +0530

    feat: Biometric Function Development - Develop the `onEnableBiometric` function to check Biometric Support in the Device - Develop the `onLoginWithBiometrics` function to receive the signature

--- src/view/screens/settings/index.tsx
+++ src/view/screens/settings/index.tsx
@@ -1,13 +1,50 @@
 import {Box, Button, VStack} from 'native-base';
 import * as React from 'react';
+import {NativeModules} from 'react-native';
+import {useDispatch} from 'react-redux';
+
+import * as Keychain from 'react-native-keychain';
 
 export interface Props {}
 
+const {RNSettingsOpen} = NativeModules;
+
 const Settings: React.FC<Props> = () => {
+  const dispatch = useDispatch();
+
+  const onEnable = () => {
+    const type = Keychain.getSupportedBiometryType();
+
+    console.log('type', type);
+  };
+
+  // const onEnableBiometric = async () => {
+  //   let {available, biometryType} = await rnBiometrics.isSensorAvailable();
+  //   if (available && biometryType === BiometryTypes.TouchID) {
+  //     console.log('TouchID is supported', biometryType);
+  //   } else if (available && biometryType === BiometryTypes.FaceID) {
+  //     console.log('FaceID is supported', biometryType);
+  //   } else if (available && biometryType === BiometryTypes.Biometrics) {
+  //     console.log('Biometric is supported', biometryType);
+  //   } else {
+  //     dispatch(biometricStatusSetter(false));
+
+  //     return Platform.OS === 'ios'
+  //       ? await Linking.openSettings()
+  //       : await RNSettingsOpen.openSettings();
+  //   }
+
+  //   const {publicKey} = await rnBiometrics.createKeys();
+
+  //   console.log('publicKey', publicKey);
+
+  //   dispatch(biometricStatusSetter(true));
+  // };
+
   return (
     <VStack justifyContent="center" alignItems="center" flex={1} safeArea>
       <Box maxW="xs">
-        <Button onPress={() => {}}>Enable Biometric Authentication</Button>
+        <Button onPress={onEnable}>Enable Biometric Authentication</Button>
       </Box>
     </VStack>
   );
--- src/view/screens/splash/index.tsx
+++ src/view/screens/splash/index.tsx
@@ -1,42 +1,99 @@
-import {Button, Center, Input, Stack, VStack} from 'native-base';
+import {Button, Center, Input, Skeleton, Stack, VStack} from 'native-base';
 import * as React from 'react';
-import {splashLaunched} from '../../../../shared/redux/actionCreators/app';
+import ReactNativeBiometrics from 'react-native-biometrics';
+import {useDispatch} from 'react-redux';
+import {
+  biometricStatusSetter,
+  splashLaunched,
+} from '../../../../shared/redux/actionCreators/app';
+import {useTypedSelector} from '../../../../shared/utilities/useTypeSelector';
 
 import {tabbedNavigation} from '../../../navigators/navigation';
 
 interface Props {}
 
+const rnBiometrics = new ReactNativeBiometrics({allowDeviceCredentials: true});
+
 const Splash: React.FC<Props> = () => {
-  React.useEffect(() => {
-    splashLaunched();
-  }, []);
+  const dispatch = useDispatch();
+  const {biometricStatus} = useTypedSelector(state => state.app);
 
   const navigateToHome = () => {
     tabbedNavigation();
   };
 
+  const biometricKeysChecker = async () => {
+    const {keysExist} = await rnBiometrics.biometricKeysExist();
+
+    console.log('keysExist', keysExist);
+
+    dispatch(biometricStatusSetter(keysExist));
+  };
+
+  const onLoginWithBiometrics = async () => {
+    let epochTimeSeconds = Math.round(new Date().getTime() / 1000).toString();
+    let payload = epochTimeSeconds + 'some message';
+
+    console.log('payload', payload);
+
+    rnBiometrics
+      .createSignature({
+        promptMessage: 'Sign in',
+        payload: payload,
+      })
+      .then(resultObject => {
+        console.log('resultObject', resultObject);
+      })
+      .catch(reason => {
+        console.log('reason', reason);
+      });
+
+    // if (success) {
+    //   console.log('signature', signature);
+    //   navigateToHome();
+    // }
+  };
+
+  React.useEffect(() => {
+    splashLaunched();
+    biometricKeysChecker();
+  }, []);
+
   return (
     <VStack justifyContent="center" flex={1} safeArea>
-      <Center>
-        <Stack space={4} w="100%" alignItems="center">
-          <Input
-            w={{
-              base: '75%',
-              md: '25%',
-            }}
-            placeholder="Name"
-          />
-          <Input
-            w={{
-              base: '75%',
-              md: '25%',
-            }}
-            type="password"
-            placeholder="Password"
-          />
-          <Button onPress={navigateToHome}>Log In</Button>
-        </Stack>
-      </Center>
+      <Skeleton isLoaded={biometricStatus !== null}>
+        <Center>
+          {biometricStatus ? (
+            <Stack space={4}>
+              <Button onPress={onLoginWithBiometrics}>
+                Login with Biometrics
+              </Button>
+              <Button variant="subtle">Login with Pin</Button>
+            </Stack>
+          ) : (
+            <Stack space={4} w="100%" alignItems="center">
+              <Input
+                w={{
+                  base: '75%',
+                  md: '25%',
+                }}
+                placeholder="Name"
+                value="yushan"
+              />
+              <Input
+                w={{
+                  base: '75%',
+                  md: '25%',
+                }}
+                type="password"
+                placeholder="Password"
+                value="pass"
+              />
+              <Button onPress={navigateToHome}>Log In</Button>
+            </Stack>
+          )}
+        </Center>
+      </Skeleton>
     </VStack>
   );
 };
